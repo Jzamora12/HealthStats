bd1981dbaa258fe2189f7564f53f0a316c7131d4
Jzamora12 <jzamora12@mail.csuchico.edu>
2018-03-05 15:47:58 -0800

    will display size of bpm, the last value in bpm, and prints list of all things in list.

--- main.cpp
+++ main.cpp
@@ -9,6 +9,19 @@ int main()
     vector <int> bpm;
     vector <int> steps;
     vector <int> calories;
+    
+    //bpm.resize(10);
+    bpm.push_back(90);
+    bpm.push_back(100);
+    bpm.push_back(70);
+    
+    cout<< "bpm size is currently size "<<bpm.size()<<endl;
+    cout<< "The last item in bpm is... "<<bpm[ bpm.size()-1 ]<<endl;
+    cout<< "The items in the list are...\n";
 
+    for ( int i=0; i< bpm.size() ; i++) 
+        {
+            cout<<bpm[i]<<endl;
+        }
     return 0;
 }
